2fa4645eb72263f2e4c52969b854166b071966d0
Hrishikesh Ghodki <rischi98@gmail.com>
2021-11-25 14:13:37 +0100

    Last additional test for sample velocity

--- src/test_sampling_velocity.py
+++ src/test_sampling_velocity.py
@@ -5,7 +5,9 @@ import numpy as np
 
 def test():
     sim_domain = sMACGrid(domain=4,scale=4)
-    particles = [Particle(position=np.array([1.125,1.125,1.125]),velocity=np.zeros(3))]#, Particle(position=np.array([2.625,1.375,1.875]),velocity=np.zeros(3))]
+    test_particle = Particle(position=np.array([1.125,1.125,1.125]),velocity=np.zeros(3))
+    test_particle2 = Particle(position=np.array([1.625,1.375,1.875]),velocity=np.zeros(3))
+    particles = [test_particle,test_particle2]
     grid_size = sim_domain.grid_size
     
     test_velocities_X = np.arange(grid_size**2*(grid_size+1)).reshape(grid_size+1,grid_size,grid_size)
@@ -16,9 +18,9 @@ def test():
     interpolated_vel = sim_domain.sample_velocity(particles=particles)
     print(interpolated_vel)
     
-    test_particle = Particle(position=np.array([1.125,1.125,1.125]),velocity=np.zeros(3))
-    sim_domain.grid_to_particles([test_particle])
+    sim_domain.grid_to_particles([test_particle,test_particle2])
     print(test_particle.v)
+    print(test_particle2.v)
     return
 
 if __name__ == '__main__':
